a3ae3d408df164e15bce1454c1c9ad67ecada784
Pedro Mendonça <pgcalixto@gmail.com>
2017-08-31 08:17:24 -0300

    Revert "Refactored mastermind module conforming to I/O spec"
    This reverts commit 72fa74a09049572ad0d33ebbee4d65a166b1d150.

--- mastermind.pl
+++ mastermind.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/swipl
+% #!/usr/bin/swipl
 
 :- module(mastermind, [play/0]).
 
@@ -8,47 +8,42 @@
 :- use_module(pool).
 
 /**
- * first_try(-Correct, -Regular)
+ * first_try(+Answer, +Guess, -Correct, -Regular)
  *
  * Performs the first guess of the player (1,2,3,4).
  */
-first_try(Correct, Regular) :-
-    write([1,2,3,4]), nl,
-    read([Correct, Regular]).
+first_try(Answer, Guess, _, _) :-
+    check_answer:check_answer(Answer, Guess, 4, 0),
+    write('ganhei'), nl, halt.
+
+first_try(Answer, Guess, Correct, Regular) :-
+    check_answer:check_answer(Answer, Guess, Correct, Regular).
 
 /**
- * try(+Pool, -Correct, -Regular)
+ * try(+Answer, +Pool, -Correct, -Regular)
  *
  * Given the current pool, the last feedback of Correct and Regular elements,
  * performs the steps needed to try to win the game.
  */
-try(Pool, Correct, Regular) :-
+try(Answer, Pool, Correct, Regular) :-
+    player:make_guess(Pool, Correct, Regular, Guess),
+    check_answer:check_answer(Answer, Guess, Correct1, Regular1),
 
-    (Pool = [], write('erro'), nl, halt);
     (
-     player:make_guess(Pool, Correct, Regular, Guess),
-     write(Guess), nl,
-     read([Correct1, Regular1]),
-
+     (Correct1 = 4, Regular1 = 0, write('ganhei'), nl, halt) ;
      (
-      (Correct1 = 4, Regular1 = 0, write('ganhei'), nl, halt) ;
-      (
-       pool:update_pool(Pool, Guess, Correct1, Regular1, NewPool),
-       try(NewPool, Correct1, Regular1)
-      )
+      pool:update_pool(Pool, Guess, Correct1, Regular1, NewPool),
+      try(Answer, NewPool, Correct1, Regular1)
      )
     ).
 
 play :-
-    FirstGuess = [1,2,3,4],
-    first_try(Correct, Regular),
-    (
-     (Correct = 4, Regular = 0, write('ganhei'), halt);
-     (
-      pool:pool(Pool),
-      pool:update_pool(Pool, FirstGuess, Correct, Regular, NewPool),
-      try(NewPool, Correct, Regular)
-     )
-    ).
+    answer_generator:answer(Answer),
 
-:- play, halt.
+    FirstGuess = [1,2,3,4],
+    first_try(Answer, FirstGuess, Correct, Regular),
+    pool:pool(Pool),
+    pool:update_pool(Pool, FirstGuess, Correct, Regular, NewPool),
+    try(Answer, NewPool, Correct, Regular).
+
+:- play.
