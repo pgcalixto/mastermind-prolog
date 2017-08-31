5723a6af51d678f3321da542ffb4566b384a4a2d
Pedro Mendonça <pgcalixto@gmail.com>
2017-08-31 06:35:46 -0300

    Added mastermind module functors documentation

--- mastermind.pl
+++ mastermind.pl
@@ -7,6 +7,11 @@
 :- use_module(player).
 :- use_module(pool).
 
+/**
+ * first_try(+Answer, +Guess, -Correct, -Regular)
+ *
+ * Performs the first guess of the player (1,2,3,4).
+ */
 first_try(Answer, Guess, _, _) :-
     check_answer:check_answer(Answer, Guess, 4, 0),
     write('ganhei'), nl, halt.
@@ -14,6 +19,12 @@ first_try(Answer, Guess, _, _) :-
 first_try(Answer, Guess, Correct, Regular) :-
     check_answer:check_answer(Answer, Guess, Correct, Regular).
 
+/**
+ * try(+Answer, +Pool, -Correct, -Regular)
+ *
+ * Given the current pool, the last feedback of Correct and Regular elements,
+ * performs the steps needed to try to win the game.
+ */
 try(Answer, Pool, Correct, Regular) :-
     player:make_guess(Pool, Correct, Regular, Guess),
     check_answer:check_answer(Answer, Guess, Correct1, Regular1),
@@ -28,8 +39,6 @@ try(Answer, Pool, Correct, Regular) :-
 
 play :-
     answer_generator:answer(Answer),
-
-    write('answer: '), write(Answer), nl,
 
     FirstGuess = [1,2,3,4],
     first_try(Answer, FirstGuess, Correct, Regular),
